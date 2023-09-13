1ae45d624ab57ee6b78ded33ca0c7eca8ee37d75
omaomach <aaronjoash44@gmail.com>
2023-09-13 15:51:46 +0300

    added onClickLister to CardView

--- app/src/main/java/com/omao/topgamesapp/GameAdapter.kt
+++ app/src/main/java/com/omao/topgamesapp/GameAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class GameAdapter(val gameList: ArrayList<GameModel>): RecyclerView.Adapter<GameAdapter.MyViewHolder>() {
@@ -17,7 +18,13 @@ class GameAdapter(val gameList: ArrayList<GameModel>): RecyclerView.Adapter<Game
             gameImage = itemView.findViewById(R.id.cardViewImage)
             gameTitle = itemView.findViewById(R.id.cardViewText)
 
-//            itemView.setOnClickListener()
+            itemView.setOnClickListener() {
+                Toast.makeText(
+                    itemView.context,
+                    "You Chose: ${gameList[adapterPosition].textCard}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
 
         }
 
--- app/src/main/java/com/omao/topgamesapp/MainActivity.kt
+++ app/src/main/java/com/omao/topgamesapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.omao.topgamesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // AdapterView: Recycler  (CardView)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        // 2- Data Source: List of Game Objects
+        var gamesList: ArrayList<GameModel> = ArrayList()
+
+        val game1 = GameModel(R.drawable.card1, "Horizon Chase")
+        val game2 = GameModel(R.drawable.card2, "PUBG")
+        val game3 = GameModel(R.drawable.card3, "Head Ball 2")
+        val game4 = GameModel(R.drawable.card4, "FIFA 22")
+        val game5 = GameModel(R.drawable.card5, "Fortnite")
+        val game6 = GameModel(R.drawable.card6, "Hooked on You")
+
+        gamesList.add(game1)
+        gamesList.add(game2)
+        gamesList.add(game3)
+        gamesList.add(game4)
+        gamesList.add(game5)
+        gamesList.add(game6)
+
+        // Adapter
+        val adapter = GameAdapter(gamesList)
+        recyclerView.adapter = adapter
+
     }
 }
